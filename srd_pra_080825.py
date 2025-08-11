3054f86b6eb623f829b263139f7bf12b6ad92302
Venkata Ramana Murthy Maddali <vrmaddali@hotmail.com>
2025-08-11 13:35:14 +0530

    assignment3_pt1

--- srd_pra_080825.py
+++ srd_pra_080825.py
@@ -3,7 +3,24 @@
 # print(numbers)
 
 
-numbers = [1, 2, 3]
-new_numbers = numbers.copy()
-new_numbers.append(4)
-print(new_numbers)
+# numbers = [1, 2, 3]
+# new_numbers = numbers.copy()
+# new_numbers.append(4)
+# print(new_numbers)
+
+# name  = "Venkata Ramana Murthy Maddali"                          #string     
+# age = 66                                                         #Intiger                                           #Intiger
+# weight_in_kgs = 106.6                                            #float
+# is_he_retired = True                                             #boolean
+# print(type (name))
+# print(type (age))
+# print(type (weight_in_kgs))       
+# print(type (is_he_retired))
+
+#rice_bag_weight_in_kgs = 70.56
+#print(type(rice_bag_weight_in_kgs))                      #<class 'float'>
+#rice_bag_weight_in_kgs = int(rice_bag_weight_in_kgs)      #<class 'int'>     
+#print(rice_bag_weight_in_kgs)                             #70  output
+#rice_bag_weight_in_kgs = str(rice_bag_weight_in_kgs)
+#print(type(rice_bag_weight_in_kgs))                        #<class 'str'>
+#print(rice_bag_weight_in_kgs)                              #70.56    output
